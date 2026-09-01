507ce3d3ce5141d28b6592d66840e84dbf6755a7
Icarus <turjo.cache@gmail.com>
2026-09-01 11:49:08 +0600

    bugfix

--- src/main/java/com/optimizely/scheduler/config/SecurityConfig.java
+++ src/main/java/com/optimizely/scheduler/config/SecurityConfig.java
@@ -25,10 +25,12 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class SecurityConfig {
 
     private final JwtFilter jwtFilter;
+    private final CorsConfig corsConfig;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+            .cors(cors -> cors.configurationSource(corsConfig.corsConfigurationSource()))
             .csrf(csrf -> csrf.disable())
             .sessionManagement(session ->
                 session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
